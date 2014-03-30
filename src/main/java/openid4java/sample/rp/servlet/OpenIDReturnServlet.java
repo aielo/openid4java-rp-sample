79c0598a7f77af49acd2ca4257c88c976f1bf22f
Ricardo Aielo <ricardo.aielo@gmail.com>
2014-03-30 03:35:36 -0300

    auth return servlet and view

---
+++ src/main/java/openid4java/sample/rp/servlet/OpenIDReturnServlet.java
@@ -0,0 +1,79 @@
+package openid4java.sample.rp.servlet;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import openid4java.sample.rp.helper.ConsumerManagerProvider;
+
+import org.openid4java.consumer.ConsumerManager;
+import org.openid4java.consumer.VerificationResult;
+import org.openid4java.discovery.DiscoveryInformation;
+import org.openid4java.discovery.Identifier;
+import org.openid4java.message.ParameterList;
+
+@WebServlet(name="return", displayName="return", urlPatterns={"/return"}, loadOnStartup=1)
+public class OpenIDReturnServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -2886666363853721825L;
+	private ConsumerManager cm = ConsumerManagerProvider.getConsumerManager();
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		this.doPost(req, res);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		// auth response info
+		Map<Object, Object> ri = new HashMap<Object, Object>();
+
+		// requested URL
+		String url = req.getRequestURL().toString();
+
+		// parameter map
+		Map<String, String> pm = new HashMap<String, String>();
+		for (String p : req.getParameterMap().keySet()) {
+			if (req.getParameterMap().get(p).length == 1) {
+				pm.put(p, req.getParameterMap().get(p)[0]);
+			} else {
+				pm.put(p, Arrays.toString(req.getParameterMap().get(p)));
+			}
+		}
+
+		// view info
+		ri.put("return_url", url);
+		ri.put("return_parameters", pm);
+		req.setAttribute("info", ri);
+
+		try {
+			DiscoveryInformation di = (DiscoveryInformation) req.getSession().getAttribute("openid4java-rp-sample.discovery");
+			VerificationResult vr = cm.verify(url, new ParameterList(req.getParameterMap()), di);
+			Identifier i = vr.getVerifiedId();
+			ri.put("verified", i != null);
+			if (i != null) {
+				ri.put("id", i.getIdentifier());
+			}
+			ri.put("return_parameters", vr.getAuthResponse().getParameterMap());
+
+			// reseting session
+			req.getSession().removeAttribute("openid4java-rp-sample.discovery");
+			req.getSession().removeAttribute("openid4java-rp-sample.request");
+
+		} catch (Throwable t) {
+			// view error
+			req.setAttribute("error", t.getMessage());
+		}
+
+		// show view
+		req.getRequestDispatcher("WEB-INF/return.jsp").forward(req, res);
+	}
+}
---
+++ src/main/webapp/WEB-INF/return.jsp
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+	<head>
+		<meta charset="utf-8">
+		<meta name="author" content="Ricardo Aielo">
+		<title>Auth Return Information - OpenID RP Sample</title>
+	</head>
+	<body>
+		<c:if test="${not empty error}">
+			<h1>Error</h1>
+			<c:out value="${error}" />
+			<h3>Return URL</h3>
+			<c:out value="${info.return_url}" />
+			<br />
+			<h3>Return parameters</h3>
+			<c:forEach var="p" items="${info.return_parameters}">
+				<c:out value="${p.key}" />: <c:out value="${p.value}" />
+				<br />
+			</c:forEach>
+
+		</c:if>
+
+		<c:if test="${empty error}">
+			<h1>Auth Response</h1>
+			<h3>id:</h3> <c:out value="${info.id}" />
+			<br />
+			<h3>verified:</h3> <c:out value="${info.verified}" />
+			<br />
+			<h3>Return parameters</h3>
+			<c:forEach var="p" items="${info.return_parameters}">
+				<c:out value="${p.key}" />: <c:out value="${p.value}" />
+				<br />
+			</c:forEach>
+		</c:if>
+	</body>
+</html>
